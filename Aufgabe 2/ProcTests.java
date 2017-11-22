a53b72cb93213e94ed4b295710d41cff83274604
Asphal <33759622+Asphal@users.noreply.github.com>
2017-11-22 23:46:57 +0100

    Update ProcTests.java
    Hatte die falsche Datei hochgeladen... ups. Sollte nun passen.

--- Aufgabe 2/ProcTests.java
+++ Aufgabe 2/ProcTests.java
@@ -1,55 +1,59 @@
-import static org.junit . Assert .*;
-import org. junit . Test ; 
+import static org.junit.Assert.*;
+import org.junit.Test; 
 
 public class ProcTests {
-	/*Der erste Parameter dabei ist
-    eine kurze Beschreibung des Testfalls (optional), danach folgt der erwartete Wert
-    und der zu testende Ausdruck */
-    //so is(beschreibung,erwartet,testausdruck)
+
+
   @Test
     public void testZuTake1 () {
-        int [] eingabe = {1,2,3,4}; 
-        int x = 2;
-        int [] erwartet = {1,2};
-        assertArrayEquals (erwartet,Procs.take(eingabe,x));
+        int [] eingabe = {1 , 2 , 3 , 4}; 
+        int x = 0;
+        int [] erwartet = {};
+        assertArrayEquals ("n = 0", Procs.take(eingabe,x) , erwartet);
     }
 
     @Test
     public void testZuTake2 () {
-        int [] eingabe ={1,2,3,4};
+        int [] eingabe = new int[4];
         int x = 4;
-        int [] erwartet ={1,2,3,4};
-        assertArrayEquals (erwartet,Procs.take(eingabe,x));
+        int [] erwartet = new int [4];
+        assertArrayEquals ("leeres Array", Procs.take(eingabe,x) , erwartet);
+    }
+
+    @Test
+    public void testZuTake3 () {
+        int [] eingabe = {1 , 2 , 3 , 4};
+        int x = 2;
+        int [] erwartet = {1 , 2};
+        assertArrayEquals ("leeres Array", Procs.take(eingabe,x) , erwartet);
     }
 
     @Test
     public void testZuReverse1 () {
-        int [] eingabe={1,2,3,4,5,6};
+        int [] eingabe = new int [1];
         Procs.reverse( eingabe );
-        int [] erwartet={6,5,4,3,2,1};
-        assertArrayEquals (erwartet,eingabe);
+        int [] erwartet = new int [1];
+        assertArrayEquals ("ar.length = 1", eingabe , erwartet);
     }
 
     @Test
     public void testZuReverse2 () {
-        int [] eingabe = {1,1,1,2,3};
+        int [] eingabe = {1 , 1 , 1 , 2 , 3};
         Procs.reverse(eingabe);
-        int [] erwartet = {3,2,1,1,1};
-        assertArrayEquals (erwartet,eingabe);
+        int [] erwartet = {3 , 2 , 1 , 1 , 1};
+        assertArrayEquals ("ar.length = 5", eingabe , erwartet);
     } 
 
     @Test
     public void testZuRepeat1 () {
-        //len < snippet.length
-        int [] eingabe = { 1 , 2 , 3 , 4 };
-        int [] erwartet = { 1 , 2 };
-        assertArrayEquals (erwartet,Procs.repeat(eingabe,2));
+        int [] eingabe = {1 , 2 , 3 , 4};
+        int [] erwartet = {1 , 2};
+        assertArrayEquals ("len < snippet.length", Procs.repeat(eingabe , 2) , erwartet);
     }
 
     @Test
     public void testZuRepeat2 () {
-        //len >snippet.length
-        int [] eingabe = { 1 , 2 , 3 , 4 };
+        int [] eingabe = {1 , 2 , 3 , 4};
         int [] erwartet = new int[6];
         erwartet[0] = 1;
         erwartet[1] = 2;
@@ -57,14 +61,14 @@ public class ProcTests {
         erwartet[3] = 4;
         erwartet[4] = 1;
         erwartet[5] = 2;
-        assertArrayEquals (erwartet,Procs.repeat(eingabe,6)); 
-    } 
+        assertArrayEquals ("len > snippet.length", Procs.repeat(eingabe , 6) , erwartet); 
+    }
+
     @Test
     public void testZuRepeat3 () {
-        //len = snippet.length
-        int [] eingabe = {1,2,3,4};
-        int [] erwartet = {1,2,3,4};
-        assertArrayEquals (erwartet,Procs.repeat(eingabe,4));
-    }
+        int [] eingabe = {1 , 2 , 3 , 4};
+        int [] erwartet = {1 , 2 , 3 , 4};
+        assertArrayEquals ("len = snippet.length", Procs.repeat(eingabe , 4) , erwartet);
+    } 
 
 }
